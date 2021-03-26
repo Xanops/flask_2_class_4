79c5c7bf20eba4cc30729c7224cc0a7b5a2cdea8
Xanos <74895133+Xanops@users.noreply.github.com>
2021-03-26 15:33:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ sl = {'title': 'Анкета',
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('index.html', title=title)
+    return render_template('base.html', title=title)
 
 
 @app.route('/answer')
